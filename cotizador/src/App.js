2b63d1c6f3cfb29893d15c0ed13a292e175eccc7
nelsonPla <65683458+nelsonPla@users.noreply.github.com>
2020-06-10 18:51:16 -0600

    Primer cambio
    Styled component

---
+++ cotizador/src/App.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import Header from './components/Header';
+import styled from '@emotion/styled';
+import Formulario from './components/Formulario';
+
+
+const Contenedor = styled.div`
+	max-width: 600px;
+	margin: 0 auto;
+`;
+
+const ContenedorFormulario = styled.div`
+	background-color: #FFF;
+	padding: 3rem;
+
+`;
+
+function App() {
+    return (
+    	<Contenedor>
+    		<Header 
+    			titulo = 'Cotizador de Seguros'
+    		/>
+    		<ContenedorFormulario>
+    			<Formulario />
+    		</ContenedorFormulario>
+    	</Contenedor>
+    );
+}
+
+export default App;
